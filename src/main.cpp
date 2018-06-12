eb2b89b394f6ccc21c06c32f0b81a50063b5c355
jpow <powelj87@gmail.com>
2018-06-11 20:45:18 -0400

    Created source code directory and moved source code into directory

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <unistd.h>\
+
+#include <gtest/gtest.h>
+/*
+int factorial(int number){
+  if(number == 1)
+    return 1;
+  else
+    number * factorial(number - 1);
+}
+
+
+TEST(Test1, checksFactorial){
+  ASSERT_EQ(1,factorial(1));
+}
+
+
+int main(int argc, char *argv[])
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+
+
+}
+*/
+
+#include <iostream>
+
+int main(){
+  std::cout << "Hello World" << std::endl;
+  return 0;
+}
