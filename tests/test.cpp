047a45d6bc1a14aa5d021422c5635e78d3c1e462
Jordan Powell <powelj87@gmail.com>
2018-06-27 17:23:47 -0400

    Updated test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,9 +1,14 @@
 #include <gtest/gtest.h>
 #include "../src/factorial.cpp"
 
-TEST(testOne, factorial){
+
+TEST(factorial, Positive){
   EXPECT_EQ(120, factorial(5));
   EXPECT_EQ(6, factorial(3));
+}
+
+TEST(factorial, zero){
+  EXPECT_EQ(1, factorial(0));
 }
 
 TEST(testTwo, addition){
@@ -11,9 +16,14 @@ TEST(testTwo, addition){
   EXPECT_EQ(100, add(50,50));
 }
 
+TEST(testThree, greatestCommonDenominator){
+  EXPECT_EQ(0, gcd(0, 5));
+  EXPECT_EQ(4, gcd(8, 12));
+            
+}
+
 int main(int argc, char *argv[])
 {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
-  
 }
