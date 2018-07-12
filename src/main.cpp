474a2f5978d27f29d0a9789f4e836e0cd39569ca
Jordan Powell <powelj87@gmail.com>
2018-07-12 15:17:26 -0400

    wrote some test code beginning the development of my project

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main(){
-  std::cout << "Hello World" << std::endl;
+  //create fork process
+  pid_t pid = fork();
+  //store command
+  std::string command;
+  int returnCode;
 
+  while (std::cin >> command) {
+
+    if (command == "exit")
+      break;
+    returnCode = system(command.c_str());
+    
+  }
+
+  exit(1);
+  
   return 0;
 }
