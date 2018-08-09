7b713734f9546ecc1daccc0777a3eabb24c18bc2
Jordan Powell <powelj87@gmail.com>
2018-08-09 15:27:35 -0400

    added correct/incorrect feature; added istream reader for text files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,55 @@
 #include <iostream>
 #include <unistd.h>
 #include <stdlib.h>
+#include <fstream>
 #include "color.hpp"
 
 
 int main(){
+
+  //Print Logo read from file
+  std::ifstream istr;
+  istr.open("locale/en/logo.txt");
+  std::string output;
+  while (!istr.eof()) {
+    
+    std::getline(istr, output);
+    std::cout << fgColor::ORANGE << output << std::endl;
+  }
+
+  //Reset the color values
+  std::cout << fgColor::RESET << std::endl;
+    
+  //get hostname from system
+  char hostname[100];
+  gethostname(hostname, 100);
+
+  //get login name from system
+  char loginName[100];
+  cuserid(loginName);
+
   //store command
   std::string command;
-  int returnCode;
-
-  std::cout << "User ID: " << getuid() << std::endl;
-  std::cout << "Process ID: " << getpid() << std::endl;
-
+   
   do{
   
     
     //Print prompt
-    std::cout << fgColor::ORANGE << "username" << fgColor::GREEN << "@hostname " << fgColor::RED << get_current_dir_name() << fgColor::GREY << " $ ";
+    std::cout << fgColor::RED << loginName << fgColor::GREEN << "@" << hostname << " " << fgColor::WHITE << get_current_dir_name() << fgColor::GREY << " $ ";
     
     //read input
-    std::cin >> command;
-
+    std::getline(std::cin, command);
+    
     if(command == "cd") {
       std::cin >> command;
       chdir(command.c_str());
     }else {
       system(command.c_str());
+      std::string addMargin = "\t\t\t\t\t";
+      if(command == "ls")
+      std::cout << bgColor::GREEN << addMargin << "Great Work" << addMargin << bgColor::RESET << std::endl;
+      else
+      std::cout << bgColor::RED << addMargin << "Try Again" << addMargin << bgColor::RESET << std::endl;
     }
 
     
